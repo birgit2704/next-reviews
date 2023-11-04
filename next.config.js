f49c012f3e1db18e9907add1f9b41bef159835dc
Birgit <birgit.bartenschlager@web.de>
2023-11-04 17:47:34 +0100

    environment variables

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
+const url = new URL(process.env.CMS_IMAGE_PATTERN);
+
 /** @type {import {'next'}.NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-    ],
+    remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(":", ""),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
